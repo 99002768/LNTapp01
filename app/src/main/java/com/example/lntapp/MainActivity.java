660391fc814163d7f3d0a0de1a582844000af02c
thrinath <Thrinath.Thota@ltts.com>
2020-11-10 12:21:05 +0530

    added pass statements and dailer

--- app/src/main/java/com/example/lntapp/MainActivity.java
+++ app/src/main/java/com/example/lntapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lntapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,21 +19,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e(TAG,"onCreate");
+        Log.i(TAG,"onCreate");
     }
 
     public void clickhandler(View view) {
 //        Toast.makeText(this, "hii thrinath", Toast.LENGTH_SHORT).show();
-        EditText text;
-        text=findViewById(R.id.Name);
-        String thrinath=text.getText().toString();
+//        EditText text;
+//        text=findViewById(R.id.Name);
+//        String thrinath=text.getText().toString();
 
 //        TextView thrinath;
 //        thrinath=findViewById(R.id.thrinath);
 //        thrinath.setText(input);
-        Intent intent=new Intent(MainActivity.this,HomeActivity.class);
-       intent.putExtra("mykey","sfdgjsfg");
+        switch (view.getId()){
+            case R.id.button:
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("tel:12345678 "));
+                startActivity(intent);
+                break;
+            case R.id.button2:
+                startmethod();
+                break;
+        }
+
+    }
+
+    private void startmethod() {
+        Intent intent=new Intent(MainActivity.this, HomeActivity.class);
+        intent.putExtra("mykey","sfdgjsfg");
+        int c=add(10,20);
 // intent.putExtra("mykey",thrinath);
         startActivity(intent);
+    }
+
+    private int add(int i, int i1) {
+        return  0;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"onstart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"onstop");
     }
 }
