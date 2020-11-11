a64dabb33030bb9b73e90104dda8defb9f43b38f
thrinath <Thrinath.Thota@ltts.com>
2020-11-11 10:52:23 +0530

    added RecyclerView

---
+++ app/src/main/java/com/example/lntapp/Adapter.java
@@ -0,0 +1,72 @@
+package com.example.lntapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * is to put data into each row of the listview
+ */
+public class Adapter  extends RecyclerView.Adapter<Adapter.wordViewHolder>{
+
+    String[] languages;
+    LayoutInflater layoutInflater;
+
+    public Adapter(Context context, String[] languagesData) {
+        languages = languagesData;
+        layoutInflater =LayoutInflater.from(context);
+
+    }
+
+    /**
+     * onCreateViewHolder job  is to buy wooden planks
+     * @param parent
+     * @param viewType
+     * @return
+     */
+
+    @NonNull
+    @Override
+    public Adapter.wordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View rowView = layoutInflater.inflate(R.layout.row_listview,parent,false);
+        return new wordViewHolder(rowView);
+    }
+
+    /**
+     * onBindViewHolder job is write data on the planks
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.wordViewHolder holder, int position) {
+
+        holder.titleTextView.setText(languages[position]);
+
+    }
+
+    /**
+     * it will keep the count of number of data items in the dataset
+     * @return
+     */
+    @Override
+    public int getItemCount() {
+        return languages.length;
+    }
+
+    /**
+     * to hold the recycle stock and new stock of wooden planks
+     */
+    public class wordViewHolder extends RecyclerView.ViewHolder {
+        public TextView titleTextView;
+        public wordViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lntapp/RecyclerActivity.java
@@ -0,0 +1,23 @@
+package com.example.lntapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class RecyclerActivity extends AppCompatActivity {
+
+    String[] languagesData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+        languagesData = new String[]{"english","hindi","urdu","telugu"};
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        Adapter adapter = new Adapter(this,languagesData);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+}
