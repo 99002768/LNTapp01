4a66a7b8d9590c894ee32ed9cac7e2a187a3a449
thrinath <Thrinath.Thota@ltts.com>
2020-11-11 13:39:08 +0530

    added book search

--- app/src/main/java/com/example/lntapp/AsyncActivity.java
+++ app/src/main/java/com/example/lntapp/AsyncActivity.java
@@ -1,7 +1,8 @@
 package com.example.lntapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.TextView;
+import android.widget.EditText;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,18 +11,27 @@ import android.widget.ProgressBar;
 public class AsyncActivity extends AppCompatActivity {
     private static final String TAG = AsyncActivity.class.getSimpleName();
     ProgressBar progressBar;
+    EditText mBookInput;
+    TextView mTitleText,mAuthorText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_async);
         progressBar = findViewById(R.id.progressBar);
+        mBookInput = findViewById(R.id.bookInput);
+        //String queryString = mBookInput.getText().toString();
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
     }
 
     public void handleClick(View view) {
         Log.i(TAG,"handleClick");
         //download something from internet
-        DownloadTask downloadTask = new DownloadTask(progressBar);
-        downloadTask.execute("https://urlForimagetobedownloaded");
+//        DownloadTask downloadTask = new DownloadTask(progressBar);
+//        downloadTask.execute("https://urlForimagetobedownloaded");
         //show the download progress on the progressbar
+        String queryString = mBookInput.getText().toString();
+
+        new FetchBook(mTitleText, mAuthorText).execute(queryString);
     }
 }
